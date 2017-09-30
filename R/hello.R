c1fca82b1b62c84d92e61a97c96488a93ec7e4d5
jcfiallos <30953887+jcfiallos@users.noreply.github.com>
2017-09-29 23:20:57 -0500

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+mymyData <- c()
+
+# Define the UI
+ui <- bootstrapPage(plotOutput('plot'))
+
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    plot(myData, type='l', col='red')
+  })
+}
+
+myshinyplot <- function(x)
+{
+  myData <<- x
+  runApp(shinyApp(ui = ui, server = server ))
+}
